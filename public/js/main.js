93d4411b0bb7103ba4d36c0cc79b4be37bbc4915
Anmol Dhami <anmoldhami01@gmail.com>
2021-09-28 00:01:16 +0530

    Updated Api and other problems

--- public/js/main.js
+++ public/js/main.js
@@ -26,50 +26,6 @@ const getInfo = async (event) => {
       temp_real_val.innerText = arrData[0].main.temp;
       const tempMood = arrData[0].weather[0].main;
       console.log(tempMood);
-      const getCurrentDay = () => {
-        var weekday = new Array(7);
-        weekday[0] = "Sun";
-        weekday[1] = "Mon";
-        weekday[2] = "Tue";
-        weekday[3] = "Wed";
-        weekday[4] = "Thr";
-        weekday[5] = "Fri";
-        weekday[6] = "Sat";
-        let currentTime = new Date();
-        let day = weekday[currentTime.getDay()];
-        return day;
-      };
-      const getCurrentTime = () => {
-        var months = [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "June",
-          "July",
-          "Aug",
-          "Sept",
-          "Oct",
-          "Nov",
-          "Dec",
-        ];
-
-        var now = new Date();
-        var month = months[now.getMonth() + 1];
-        var date = now.getDate();
-        let hours = now.getHours();
-        let mins = now.getMinutes();
-        let period = "AM";
-        if (hours > 11) {
-          period = "PM";
-        }
-        if (mins < 10) {
-          mins = "0" + mins;
-        }
-        return `${month}2 ${date} | ${hours}:${mins}${period}`;
-      };
-      curDate.innerHTML = getCurrentDay() + " | " + getCurrentTime();
 
       //condition to check sunny or cloudy
       if (tempMood == "Clear") {
